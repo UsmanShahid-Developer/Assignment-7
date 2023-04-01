c909256b7b8f7a489fa2130024f08d78ebb1247a
UsmanShahid-Developer <usmanshahidite@gmail.com>
2023-04-01 16:31:21 +0500

    Program # 1

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+
+// ============Program # 1==============
+// Write a js program to find maximum between two numbers.
+
+
+
+var num1 = prompt("Enter a Number 1")
+var num2 = prompt("Enter a Number 2")
+if (num1>num2){
+    console.log("Num1 bara hai Num2 sy");
+} 
+else if (num1<num2){
+    console.log("Num2 bara hai Num1 sy");
+}
+if (num1==num2){
+    console.log("Both Numbers are equals");
+}
+// _________________________________________________________________________
+// _________________________________________________________________________
