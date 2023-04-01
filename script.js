047578157034b603a579282d48ec8afdf8300669
UsmanShahid-Developer <usmanshahidite@gmail.com>
2023-04-01 16:33:25 +0500

    Program # 2

--- script.js
+++ script.js
@@ -19,3 +19,29 @@ if (num1==num2){
 }
 // _________________________________________________________________________
 // _________________________________________________________________________
+
+
+// ============Program # 2==============
+// Write a js program to find maximum between three numbers.
+
+
+
+var num1 = prompt("Enter a number 1")
+var num2 = prompt("Enter a number 2")
+var num3 = prompt("Enter a number 3")
+if(num1>num2 &&num1>num3){
+    console.log("Number1 bara hy number2 aur number3 sy");
+}
+else if(num2>num1 &&num2>num3){
+    console.log("Number2 bara hy number1 aur number3 sy");
+}
+else if(num3>num1 &&num3>num2){
+    console.log("Number3 bara ha number 1 aur number2 sy");
+}
+else{
+    console.log("All Numbers Are Equals.");
+}
+// _________________________________________________________________________
+// _________________________________________________________________________
+
+
