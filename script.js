93eb3fb8e491028ad1bdbcd77257bd839966fb46
UsmanShahid-Developer <usmanshahidite@gmail.com>
2023-04-01 16:37:31 +0500

    Program # 11

--- script.js
+++ script.js
@@ -101,3 +101,38 @@ else {
 
 // _________________________________________________________________________
 // _________________________________________________________________________
+
+
+
+// ============Program # 11==============
+// Write a js program to input week number and print week day.
+
+var number = prompt("Enter a week number (1-7)")
+if(number==1){
+    console.log("Sunday")
+}
+else if(number==2){
+    console.log("Monday")
+}
+else if(number==3){
+    console.log("Tuesday")
+}
+else if(number==4){
+    console.log("Wednesday")
+}
+else if(number==5){
+    console.log("Thursday")
+}
+else if(number==6){
+    console.log("Friday")
+}
+else if(number==7){
+    console.log("Saturday")
+}
+else{
+    console.log("Your number is invalid!");
+}
+
+
+// _________________________________________________________________________
+// _________________________________________________________________________
