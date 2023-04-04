3ed10a4d88fbd2c40b554625771613a2d3b90b6c
UsmanShahid-Developer <usmanshahidite@gmail.com>
2023-04-04 04:19:48 -0700

    program # 15

--- script.js
+++ script.js
@@ -305,8 +305,10 @@
 
 // ============Program # 13==============
 //write a js program to count total number of notes in given amount.
+// var notes = prompt ("enter a number of notes.")
 
-var notes = prompt("enter a number of notes.")
+// ______________________________________________________
+// ______________________________________________________
 
 
 
@@ -328,3 +330,15 @@ var notes = prompt("enter a number of notes.")
 
 
 
+// ============Program # 15==============
+//write a js program to input allsidesof a triangel and check wheather triangle is valid or not
+
+const side1 = parseFloat(prompt("Enter the first side: "));
+const side2 = parseFloat(prompt("Enter the second side: "));
+const side3 = parseFloat(prompt("Enter the third side: "));
+
+if (side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2) {
+  console.log("The triangle is valid.");
+} else {
+  console.log("The triangle is not valid.");
+}
