98d0dd609499a5df97a7ec6726ca1bc7c566e83a
UsmanShahid-Developer <usmanshahidite@gmail.com>
2023-04-05 03:34:45 -0700

    update

--- script.js
+++ script.js
@@ -399,7 +399,7 @@
 // ______________________________________________________
 
 
-// ============Program # 21==============
+// ============Program # 19==============
 //// Write a js program to input marks of five subjects Physics, 
 // Chemistry, Biology, Mathematics and Computer. 
 // Calculate percentage and grade according to following:
@@ -410,36 +410,39 @@
 // Percentage >= 60% : Grade D
 // Percentage >= 40% : Grade E
 // Percentage < 40% : Grade F
-var Physics = + prompt("Enter thr marks of Physics:");
-var Chemistry = + prompt("Enter thr marks of Chemistry:");
-var Biology = + prompt("Enter thr marks of Biology:");
-var Mathmatics = + prompt("Enter thr marks of Mathmatics:");
-var Computer = + prompt("Enter thr marks of Computer:");
 
-var sum = (Physics + Chemistry + Biology + Mathmatics + Computer);
-var percentage = ((sum * 100) / 500);
 
-if (percentage >= 90) {
-    console.log(percentage + "%" + " and Grade A");
-}
-else if (percentage >= 80) {
-    console.log(percentage + "%" + " and Grade B");
-}
-else if (percentage >= 70) {
-    console.log(percentage + "%" + " and Grade C");
-}
-else if (percentage >= 60) {
-    console.log(percentage + "%" + " and Grade D");
-}
-else if (percentage >= 40) {
-    console.log(percentage + "%" + " and Grade E");
-}
-else if (percentage < 40) {
-    console.log(percentage + "%" + " and Grade F");
-}
-else {
- console.log ("Invalid Input!");
-}
+
+// var Physics = + prompt("Enter thr marks of Physics:");
+// var Chemistry = + prompt("Enter thr marks of Chemistry:");
+// var Biology = + prompt("Enter thr marks of Biology:");
+// var Mathmatics = + prompt("Enter thr marks of Mathmatics:");
+// var Computer = + prompt("Enter thr marks of Computer:");
+
+// var sum = (Physics + Chemistry + Biology + Mathmatics + Computer);
+// var percentage = ((sum * 100) / 500);
+
+// if (percentage >= 90) {
+//     console.log(percentage + "%" + " and Grade A");
+// }
+// else if (percentage >= 80) {
+//     console.log(percentage + "%" + " and Grade B");
+// }
+// else if (percentage >= 70) {
+//     console.log(percentage + "%" + " and Grade C");
+// }
+// else if (percentage >= 60) {
+//     console.log(percentage + "%" + " and Grade D");
+// }
+// else if (percentage >= 40) {
+//     console.log(percentage + "%" + " and Grade E");
+// }
+// else if (percentage < 40) {
+//     console.log(percentage + "%" + " and Grade F");
+// }
+// else {
+//  console.log ("Invalid Input!");
+// }
 
 
 
