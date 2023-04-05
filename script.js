294e9bb79601c6d9e302019a6602c2283f1ce859
UsmanShahid-Developer <usmanshahidite@gmail.com>
2023-04-05 03:26:54 -0700

    Program # 21

--- script.js
+++ script.js
@@ -400,32 +400,83 @@
 
 
 // ============Program # 21==============
-//Write a js program to input electricity unit charges and calculate total electricity bill according to the given condition:
-var billAmount=prompt("Enter electricity Units")
-if (billAmount<=50){
-    var amount = billAmount * 0.50;
-    var Amount = amount * (20/100)
-    var totalAmount = amount + Amount;
-    console.log("Your Bill Amount is:" , totalAmount );
+//// Write a js program to input marks of five subjects Physics, 
+// Chemistry, Biology, Mathematics and Computer. 
+// Calculate percentage and grade according to following:
+
+// Percentage >= 90% : Grade A
+// Percentage >= 80% : Grade B
+// Percentage >= 70% : Grade C
+// Percentage >= 60% : Grade D
+// Percentage >= 40% : Grade E
+// Percentage < 40% : Grade F
+var Physics = + prompt("Enter thr marks of Physics:");
+var Chemistry = + prompt("Enter thr marks of Chemistry:");
+var Biology = + prompt("Enter thr marks of Biology:");
+var Mathmatics = + prompt("Enter thr marks of Mathmatics:");
+var Computer = + prompt("Enter thr marks of Computer:");
+
+var sum = (Physics + Chemistry + Biology + Mathmatics + Computer);
+var percentage = ((sum * 100) / 500);
+
+if (percentage >= 90) {
+    console.log(percentage + "%" + " and Grade A");
 }
-else if (billAmount<=150){
-    var amount = billAmount * 0.75;
-    var Amount = amount * (20/100)
-    var totalAmount = amount + Amount;
-    console.log("Your Bill Amount is:" , totalAmount );
+else if (percentage >= 80) {
+    console.log(percentage + "%" + " and Grade B");
 }
-else if (billAmount<=250){
-    var amount = billAmount * 1.20;
-    var Amount = amount * (20/100)
-    var totalAmount = amount + Amount;
-    console.log("Your Bill Amount is:" , totalAmount );
+else if (percentage >= 70) {
+    console.log(percentage + "%" + " and Grade C");
+}
+else if (percentage >= 60) {
+    console.log(percentage + "%" + " and Grade D");
+}
+else if (percentage >= 40) {
+    console.log(percentage + "%" + " and Grade E");
+}
+else if (percentage < 40) {
+    console.log(percentage + "%" + " and Grade F");
 }
 else {
-    var amount = billAmount * 1.50;
-    var Amount = amount * (20/100)
-    var totalAmount = amount + Amount;
-    console.log("Your Bill Amount is:" , totalAmount );
+ console.log ("Invalid Input!");
 }
+
+
 
 // ______________________________________________________
 // ______________________________________________________
+
+
+
+// ============Program # 21==============
+//Write a js program to input electricity unit charges and calculate total electricity bill according to the given condition:
+// var billAmount=prompt("Enter electricity Units")
+// if (billAmount<=50){
+//     var amount = billAmount * 0.50;
+//     var Amount = amount * (20/100)
+//     var totalAmount = amount + Amount;
+//     console.log("Your Bill Amount is:" , totalAmount );
+// }
+// else if (billAmount<=150){
+//     var amount = billAmount * 0.75;
+//     var Amount = amount * (20/100)
+//     var totalAmount = amount + Amount;
+//     console.log("Your Bill Amount is:" , totalAmount );
+// }
+// else if (billAmount<=250){
+//     var amount = billAmount * 1.20;
+//     var Amount = amount * (20/100)
+//     var totalAmount = amount + Amount;
+//     console.log("Your Bill Amount is:" , totalAmount );
+// }
+// else {
+//     var amount = billAmount * 1.50;
+//     var Amount = amount * (20/100)
+//     var totalAmount = amount + Amount;
+//     console.log("Your Bill Amount is:" , totalAmount );
+// }
+
+// ______________________________________________________
+// ______________________________________________________
+
+
